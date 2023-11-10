5d827425055b5eab25360db4f632819804c7d8e2
SKDH <shinkangdonghyeok@gmail.com>
2023-11-10 00:15:31 +0000

    modify a path

--- ultralytics-main/wildLife_animal_train.py
+++ ultralytics-main/wildLife_animal_train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     # model = YOLO("yolov8s.pt") # YOLO 모델 설정
     # model.train(data="./wildLife.yaml", epochs=100, batch=16)
-    
+
     # resume
-    model = YOLO("../runs/detect/train/weights/last.pt") # .pt 파일 확인 요망
+    model = YOLO("./runs/detect/train/weights/last.pt")  # .pt 파일 확인 요망
     model.train(resume=True)
